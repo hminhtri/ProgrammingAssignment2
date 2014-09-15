972b6b9228588fd3c5f42469ad774b8a82a034f8
hminhtri <hminhtri@gmail.com>
2014-09-15 20:28:02 +0700

    Summary
    Change code

--- cacheSolve.R
+++ cacheSolve.R
@@ -9,8 +9,8 @@ cacheSolve <- function(x = matrix(), ...) {
     message("getting cached data")
     return(m)
   }
-  matrix<-x$get()
+  matrix <-x$get()
   m<-solve(matrix, ...)
   x$setmatrix(m)
-  return(m)
+  m
 }
